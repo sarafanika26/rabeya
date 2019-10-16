61688d658ec13d70aa28bd9640109e9d0618afbb
sarafanika26 <sanika9826@gmail.com>
2019-10-16 22:22:02 +0600

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,70 @@
+// Firstly, the Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDf28kork2M6U0pQrN6BY7GnWruGUoacLc",
+    authDomain: "contact1-574d3.firebaseapp.com",
+    databaseURL: "https://contact1-574d3.firebaseio.com",
+    projectId: "contact1-574d3",
+    storageBucket: "",
+    messagingSenderId: "678409228340",
+    appId: "1:678409228340:web:4289d2f6b5245697daba61"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form function here
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name'); //getInputVal('name');
+  var company = getInputVal('company');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, company, email, phone, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+
+  //redirect to home page
+
+  setTimeout(function(){
+    document.location.href="./index.html";
+  },3050);
+
+
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    company:company,
+    email:email,
+    phone:phone,
+    message:message
+  });
+}
